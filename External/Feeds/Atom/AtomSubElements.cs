6c230772a9b6908d6d555bd364c95003a2b47637
Stephen McKamey <mckamey@users.noreply.github.com>
2007-09-23 21:52:39 +0000

    - more consolidation

--- External/Feeds/Atom/AtomSubElements.cs
+++ External/Feeds/Atom/AtomSubElements.cs
@@ -92,6 +92,74 @@ namespace MediaLib.Web.Feeds.Atom
 
 	#endregion AtomContent
 
+	#region AtomDate
+
+	/// <summary>
+	/// http://tools.ietf.org/html/rfc4287#section-3.3
+	/// </summary>
+	[Serializable]
+	public class AtomDate
+	{
+		#region Fields
+
+		private DateTime value = DateTime.MinValue;
+
+		#endregion Fields
+
+		#region Init
+
+		/// <summary>
+		/// Ctor.
+		/// </summary>
+		public AtomDate() { }
+
+		#endregion Init
+
+		#region Properties
+
+		[XmlIgnore]
+		public DateTime Value
+		{
+			get { return this.value; }
+			set { this.value = value; }
+		}
+
+		[XmlText]
+		[DefaultValue(null)]
+		public string Value_Iso8601
+		{
+			get
+			{
+				if (this.value == DateTime.MinValue)
+				{
+					return null;
+				}
+				return this.value.ToString("s");
+			}
+			set
+			{
+				if (String.IsNullOrEmpty(value) ||
+					!DateTime.TryParse(value, out this.value))
+				{
+					this.value = DateTime.MinValue;
+				}
+			}
+		}
+
+		#endregion Properties
+
+		#region Object Overrides
+
+		public override string ToString()
+		{
+			return this.Value.ToString();
+		}
+
+		#endregion Object Overrides
+	}
+
+	#endregion AtomDate
+
 	#region AtomLink
 
 	/// <summary>
