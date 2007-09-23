a3c1176af92472e1691c1399d7a5b86fd6f6bff4
Stephen <Stephen@c6181104-6f44-11de-8fcd-b1e6d76e070b>
2007-09-23 21:50:20 +0000

    - consolidation of Atom files

--- trunk/External/Feeds/Atom/AtomSubElements.cs
+++ trunk/External/Feeds/Atom/AtomSubElements.cs
@@ -5,6 +5,95 @@ using System.Xml.Serialization;
 
 namespace MediaLib.Web.Feeds.Atom
 {
+	#region AtomContent
+
+	/// <summary>
+	/// http://tools.ietf.org/html/rfc4287#section-4.1.3
+	/// </summary>
+	[Serializable]
+	public class AtomContent
+	{
+		#region Fields
+
+		private string type = null;
+		private string src = null;
+		private string value = null;
+
+		#endregion Fields
+
+		#region Init
+
+		/// <summary>
+		/// Ctor.
+		/// </summary>
+		public AtomContent() { }
+
+		#endregion Init
+
+		#region Properties
+
+		[DefaultValue(AtomTextType.text)]
+		[XmlIgnore]
+		public AtomTextType TextType
+		{
+			get
+			{
+				if (String.IsNullOrEmpty(this.type))
+				{
+					return AtomTextType.text;
+				}
+				try
+				{
+					return (AtomTextType)Enum.Parse(typeof(AtomTextType), this.type, false);
+				}
+				catch
+				{
+					return AtomTextType.text;
+				}
+			}
+			set { this.type = value.ToString(); }
+		}
+
+		[DefaultValue(null)]
+		[XmlAttribute("type")]
+		public string Type
+		{
+			get { return this.type; }
+			set { this.type = value; }
+		}
+
+		[DefaultValue(null)]
+		[XmlAttribute("src")]
+		public string Src
+		{
+			get { return this.src; }
+			set { this.src = value; }
+		}
+
+		[XmlText]
+		[DefaultValue(null)]
+		public string Value
+		{
+			get { return this.value; }
+			set { this.value = value; }
+		}
+
+		#endregion Properties
+
+		#region Object Overrides
+
+		public override string ToString()
+		{
+			return this.Value;
+		}
+
+		#endregion Object Overrides
+	}
+
+	#endregion AtomContent
+
+	#region AtomLink
+
 	/// <summary>
 	/// http://tools.ietf.org/html/rfc4287#section-4.2.7
 	/// </summary>
@@ -64,4 +153,152 @@ namespace MediaLib.Web.Feeds.Atom
 
 		#endregion Object Overrides
 	}
+
+	#endregion AtomLink
+
+	#region AtomPerson
+
+	/// <summary>
+	/// http://tools.ietf.org/html/rfc4287#section-3.2
+	/// </summary>
+	[Serializable]
+	public class AtomPerson
+	{
+		#region Fields
+
+		private string name = null;
+		private string uri = null;
+		private string email = null;
+
+		#endregion Fields
+
+		#region Init
+
+		/// <summary>
+		/// Ctor.
+		/// </summary>
+		public AtomPerson() { }
+
+		#endregion Init
+
+		#region Properties
+
+		[XmlElement("name")]
+		public string Name
+		{
+			get
+			{
+				if (this.name == null)
+				{
+					return String.Empty;
+				}
+				return this.name;
+			}
+			set { this.name = value; }
+		}
+
+		[XmlElement("uri")]
+		[DefaultValue(null)]
+		public string Uri
+		{
+			get { return this.uri; }
+			set { this.uri = value; }
+		}
+
+		[XmlElement("email")]
+		[DefaultValue(null)]
+		public string Email
+		{
+			get { return this.email; }
+			set { this.email = value; }
+		}
+
+		#endregion Properties
+
+		#region Object Overrides
+
+		public override string ToString()
+		{
+			if (!String.IsNullOrEmpty(this.Email))
+			{
+				return String.Format("\"{0}\" <{1}>", this.Name, this.Email);
+			}
+			if (!String.IsNullOrEmpty(this.Uri))
+			{
+				return String.Format("\"{0}\" <{1}>", this.Name, this.Uri);
+			}
+			return String.Format("\"{0}\"", this.Name);
+		}
+
+		#endregion Object Overrides
+	}
+
+	#endregion AtomPerson
+
+	#region AtomText
+
+	/// <summary>
+	/// http://tools.ietf.org/html/rfc4287#section-3.1
+	/// </summary>
+	[Serializable]
+	public class AtomText
+	{
+		#region Fields
+
+		private AtomTextType type = AtomTextType.text;
+		private string value = null;
+
+		#endregion Fields
+
+		#region Init
+
+		/// <summary>
+		/// Ctor.
+		/// </summary>
+		public AtomText() { }
+
+		#endregion Init
+
+		#region Properties
+
+		[DefaultValue(AtomTextType.text)]
+		[XmlAttribute("type")]
+		public AtomTextType Type
+		{
+			get { return this.type; }
+			set { this.type = value; }
+		}
+
+		[XmlText]
+		[DefaultValue(null)]
+		public string Value
+		{
+			get { return this.value; }
+			set { this.value = value; }
+		}
+
+		#endregion Properties
+
+		#region Object Overrides
+
+		public override string ToString()
+		{
+			return this.Value;
+		}
+
+		#endregion Object Overrides
+	}
+
+	#endregion AtomText
+
+	#region AtomTextType
+
+	public enum AtomTextType
+	{
+		text,
+		html,
+		xhtml
+	}
+
+	#endregion AtomTextType
 }
