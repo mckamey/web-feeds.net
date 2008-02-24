07556732e2807e7214ac6081e63eebff7adfe102
Stephen McKamey <mckamey@users.noreply.github.com>
2008-02-24 16:29:48 +0000

    - auto-gen'ing the XmlSerializers assembly for better performance - fixing some RDF serialization errors - fixing some RDF ordering issues

--- WebFeeds/WebFeeds/Properties/AssemblyVersion.cs
+++ WebFeeds/WebFeeds/Properties/AssemblyVersion.cs
@@ -2,10 +2,10 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.0802.2323
+#region 1.0.0802.2408
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0802.2323")]
+[assembly: AssemblyVersion("1.0.0802.2408")]
 
 #endregion
